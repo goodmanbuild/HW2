3bc38be3deed7a9b4fd780e25e0a4a3d9050f776
goodman <goodman.build@gmail.com>
2017-01-19 19:39:34 +0800

    加入測試並且類別，並且設計測試案例 第一集買了一本_其他都沒買_價格應為_100_mult_1_is_100，紅燈

---
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+
+
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+
+
+        public PotterShoppingCartTest() {
+         
+        }
+
+        [TestMethod]
+        public void 第一集買了一本_其他都沒買_價格應為_100_mult_1_is_100() {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.Fail();
+        }
+    }
+}
